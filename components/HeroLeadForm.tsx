f5ec4180b3779037ff3905dfa5dcf030f29835e4
Jerwin Manongsong <61395841+nahadgur@users.noreply.github.com>
2026-03-04 21:00:49 +0800

    Update HeroLeadForm.tsx

--- components/HeroLeadForm.tsx
+++ components/HeroLeadForm.tsx
@@ -18,7 +18,7 @@ const TREATMENTS = [
 ];
 
 const GOOGLE_SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbz-B9H0JTI7a9Cgyn9z-pZXKnuiNm6acAn8Zb13N21qGRcpxy7EtVvlPAjpl6f7Hj3-RQ/exec';
+  'https://script.google.com/macros/s/AKfycbw0QaLFaG-XujztIC0ZyJ_DXbvlP9BHc7F2wbwOq0D9bRYijhYq8Dje_l4enKWfoUVvfg/exec';
 
 export function HeroLeadForm({ city, service }: HeroLeadFormProps) {
   const [isSubmitting, setIsSubmitting] = useState(false);
